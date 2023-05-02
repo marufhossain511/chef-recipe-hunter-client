052f793bdd21ed038f9cfdbed4455571dc72f1c0
marufhossain511 <mfmarufh@gmail.com>
2023-05-02 16:03:37 +0600

    implement shef special recipes section

--- src/Pages/ChefRecipes/ChefRecipes.jsx
+++ src/Pages/ChefRecipes/ChefRecipes.jsx
@@ -19,7 +19,16 @@ const ChefRecipes = () => {
                 <p>Experience: {years_of_experience} Year</p>
             </div>
             <h2 className='text-white text-center text-3xl my-3'>Special Recipe of {chef_name}</h2>
-            
+            <div className='grid gap-4 grid-cols-1 md:grid-cols-2 py-6 lg:grid-cols-3 md:px-4 lg:px-4'>
+    
+                
+                {
+                    special_recipes.map((data,idx)=><SpecialRecipe 
+                    key={idx}
+                    data={data}
+                    ></SpecialRecipe>)
+                }
+            </div>
             </div>
         </div>
     );
