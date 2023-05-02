6004c6c2e70da6ecf6cb40c3e106bf53e75d1200
marufhossain511 <mfmarufh@gmail.com>
2023-05-02 16:22:41 +0600

    added toaster on favorite button

--- src/Pages/ChefRecipes/SpecialRecipe/SpecialRecipe.jsx
+++ src/Pages/ChefRecipes/SpecialRecipe/SpecialRecipe.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 const SpecialRecipe = ({data}) => {
     const {recipe_name,recipe_image,recipe_ingredients,recipe_details}=data
+  const[disable,setDisable]=useState(false)
+    const handleFavorite=()=>{
+        toast.success('Added to Favorite ❤️')
+        setDisable(true)
+    }
     return (
         <div>
             <div className="card h-96 w-full bg-base-100 shadow-xl text-white">
@@ -9,12 +15,12 @@ const SpecialRecipe = ({data}) => {
             <div className="card-body">
                 <h2 className="card-title">{recipe_name}</h2>
                 <div className='flex'>
-                <p>Ingredients: {recipe_ingredients.map(i=><li>{i}</li>)}</p>
+                <p>Ingredients: {recipe_ingredients.map((i,idx)=><li key={idx}>{i}</li>)}</p>
                 <img className='h-36 w-40 object-cover rounded border-black' src={recipe_image} alt="" />
                 </div>
                 <p>Details: {recipe_details}</p>
                 <div className="card-actions justify-end">
-                <button className="btn text-white bg-lime-700 border-none">Favorite</button>
+                <button onClick={handleFavorite} className="btn text-white bg-lime-700 border-none" disabled={disable}>Favorite</button>
                 </div>
             </div>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/Router.jsx'
 import AuthProvider from './Provider/AuthProvider.jsx'
-
+import  { Toaster } from 'react-hot-toast';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
     <RouterProvider router={router}></RouterProvider>
     </AuthProvider>
+    <Toaster/>
   </React.StrictMode>,
 )
