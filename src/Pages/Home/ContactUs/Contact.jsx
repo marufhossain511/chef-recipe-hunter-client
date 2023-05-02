fb8e22f5ebe68b12f02affc689ecb545a8e1b5cf
marufhossain511 <mfmarufh@gmail.com>
2023-05-02 15:06:06 +0600

    implement contact us section on home page

--- src/Pages/Home/ContactUs/Contact.jsx
+++ src/Pages/Home/ContactUs/Contact.jsx
@@ -3,7 +3,94 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            
+            <section className="bg-base-300 py-12 text-white">
+      <div className="max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-3xl font-bold t sm:text-4xl">
+            We Can't wait to get you started
+          </h2>
+          <p className="mt-4  text-xl">
+            Have a question or just want to say hi? Send us a message and we'll
+            get back to you as soon as possible.
+          </p>
+        </div>
+        <div className="mt-12">
+          <form
+            action="#"
+            method="POST"
+            className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+          >
+            <div>
+              <label htmlFor="name" className="block ">
+                Full name
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  autoComplete="given-name"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-white rounded-md"
+                  required
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="block ">
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-white rounded-md"
+                  required
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="subject" className="block ">
+                Subject
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="subject"
+                  id="subject"
+                  autoComplete="subject"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-white rounded-md"
+                  required
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="message" className="block ">
+                Message
+              </label>
+              <div className="mt-1">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows="4"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-white rounded-md"
+                  required
+                ></textarea>
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <button
+                type="submit"
+                className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Send message
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
         </div>
     );
 };
